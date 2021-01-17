1f8c922af059eedc4f1113ca1948a3db76de6e37
Lucas M.D.R <Lucas.roussos@gmail.com>
2021-01-17 19:15:01 +0200

    Rubocop Calculator

---
+++ lesson_2/test_area.rb
@@ -0,0 +1,13 @@
+Define method with two parameters
+Add the two parameters to reach sum and return it
+end
+
+START
+
+SET method with two parameters
+PRINT sum of the give parameters
+
+END
+
+
+Define method
